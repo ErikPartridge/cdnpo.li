9a5562095871cf4394226453e7967da359b152f4
Erik A. Partridge <erik.partridge@mail.mcgill.ca>
2019-06-09 08:50:45 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='These models drive cdnpo.li, a forecasting site for Canadian politics based on polling information.',
+    author='Erik Partridge',
+    license='',
+)
